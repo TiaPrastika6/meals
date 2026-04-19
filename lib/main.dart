023eca18aeb6f2a653890a988b4d085bdd084bae
Gek Tia Prastika Dewi <gektiaprastika06@gmail.com>
2026-04-19 21:20:58 +0800

    03

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -21,7 +22,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: const SizedBox(), 
+      home: const CategoriesScreen(),
     );
   }
 }
